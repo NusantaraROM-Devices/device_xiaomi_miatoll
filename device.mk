123856bc0c29adcd7bc934686df3828cbeb6b453
Volodymyr Zhdanov <wight554@gmail.com>
2020-10-20 10:25:53 +0300

    miatoll: Build USB HAL from source
    Change-Id: Ia6b43a0af36680164d7807e6deb67da408784a6b

--- device.mk
+++ device.mk
@@ -326,5 +326,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service
+
 # Inherit proprietary targets
 $(call inherit-product, vendor/xiaomi/miatoll/miatoll-vendor.mk)
