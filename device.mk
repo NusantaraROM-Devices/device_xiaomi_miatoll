0dfc772fa31f7cc2de671be0ea6d3cc768bc0314
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-06-28 19:24:30 +0200

    miatoll: Be sure to build vndservicemanager
    * We need it for our blobs
    
    Change-Id: Ib757b97ee2c7d43fe606225366d729854ab987fc

--- device.mk
+++ device.mk
@@ -337,5 +337,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_SOONG_NAMESPACES += \
     vendor/qcom/opensource/usb/etc
 
+# Vendor service manager
+PRODUCT_PACKAGES += \
+    vndservicemanager
+
 # Inherit proprietary targets
 $(call inherit-product, vendor/xiaomi/miatoll/miatoll-vendor.mk)
