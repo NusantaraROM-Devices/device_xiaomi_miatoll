a507378f1f8ef39da9477b815e8440b1c49ededf
Edgars Cīrulis <edgarsscirulis@gmail.com>
2021-09-14 21:35:49 +0300

    miatoll: Init Nusantara
    Signed-off-by: Edgars Cīrulis <edgarsscirulis@gmail.com>

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2020-2021 The ArrowOS
+# Copyright (C) 2021 NusantaraProject
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/nad_miatoll.mk
+
+COMMON_LUNCH_CHOICES := \
+    nad_miatoll-user \
+    nad_miatoll-userdebug \
+    nad_miatoll-eng
