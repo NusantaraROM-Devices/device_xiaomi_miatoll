496c3f94e7340161535bdf202a1b6753dd05665f
Alexander Winkowski <dereference23@outlook.com>
2022-09-04 06:02:52 +0000

    miatoll: Control wfdservice64 with vendor.wfdservice prop
    Change-Id: I984c413ce2455065b717c04df02cd2d121640798

--- extract-files.sh
+++ extract-files.sh
@@ -66,6 +66,9 @@ function blob_fixup() {
         system_ext/lib64/libwfdnative.so)
             "${PATCHELF}" --remove-needed "android.hidl.base@1.0.so" "${2}"
             ;;
+        system_ext/etc/init/wfdservice.rc)
+            sed -i "/^service/! s/wfdservice$/wfdservice64/g" "${2}"
+            ;;
     esac
 }
 
